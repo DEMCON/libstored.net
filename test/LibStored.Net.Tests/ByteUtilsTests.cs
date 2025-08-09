f37776498454f6f3a310d3af1efb7f91e2101db9
guuskuiper <guus.kuiper@demcon.nl>
2025-08-09 16:13:46 +0200

    Add StoreJournal and ByteUtils tests

---
+++ test/LibStored.Net.Tests/ByteUtilsTests.cs
@@ -0,0 +1,35 @@
+﻿// // SPDX-FileCopyrightText: 2025 Guus Kuiper
+// //
+// // SPDX-License-Identifier: MIT
+
+namespace LibStored.Net.Tests;
+
+public class ByteUtilsTests
+{
+    [Fact]
+    public void WriteBigEndianUin8Test()
+    {
+        byte[] buffer = new byte[1];
+        ByteUtils.WriteUInt8(buffer, 0x1, true);
+        byte swapped = ByteUtils.ReadUInt8(buffer, false);
+        Assert.Equal(0x1, swapped);
+    }
+    
+    [Fact]
+    public void WriteBigEndianUin16Test()
+    {
+        byte[] buffer = new byte[2];
+        ByteUtils.WriteUInt16(buffer, 0x1234, true);
+        ushort swapped = ByteUtils.ReadUInt16(buffer, false);
+        Assert.Equal(0x3412u, swapped);
+    }
+    
+    [Fact]
+    public void WriteBigEndianUin32Test()
+    {
+        byte[] buffer = new byte[4];
+        ByteUtils.WriteUInt32(buffer, 0x12345678, true);
+        uint swapped = ByteUtils.ReadUInt32(buffer, false);
+        Assert.Equal(0x78563412u, swapped);
+    }
+}
---
+++ test/LibStored.Net.Tests/StoreJournalTests.cs
@@ -0,0 +1,31 @@
+﻿// // SPDX-FileCopyrightText: 2025 Guus Kuiper
+// //
+// // SPDX-License-Identifier: MIT
+
+using LibStored.Net.Synchronization;
+
+namespace LibStored.Net.Tests;
+
+public class StoreJournalTests
+{
+    [Fact]
+    public void HasChangedTest()
+    {
+        StoreJournal journal = new(new ExampleStore());
+        const uint key = 1u;
+        
+        Assert.Equal(1u, journal.Seq);
+
+        journal.Changed(key, 4);
+        
+        Assert.True(journal.HasChanged(key, 1));
+
+        for (int i = 1; i < 50; i++)
+        {
+            journal.BumpSeq(true);
+        }
+        
+        Assert.Equal(50u, journal.Seq);
+        Assert.False(journal.HasChanged(key, 2));
+    }
+}
