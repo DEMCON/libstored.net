297220d7d4ff66d3f66b51941e0be4d689e40a92
guuskuiper <guus.kuiper@demcon.nl>
2025-08-10 14:01:00 +0200

    Add DebugClient example

---
+++ examples/LibStored.Net.DebugClient/DebugClient.cs
@@ -0,0 +1,77 @@
+﻿// SPDX-FileCopyrightText: 2025 Guus Kuiper
+//
+// SPDX-License-Identifier: MIT
+
+using System.Runtime.InteropServices;
+using System.Text;
+using LibStored.Net.Protocol;
+using LibStored.Net.ZeroMQ;
+
+namespace LibStored.Net.DebugClient;
+
+public class DebugClient : ProtocolLayer
+{
+    private static readonly TimeSpan Timeout = TimeSpan.FromSeconds(2);
+    private readonly ZeroMQLayer _layer;
+    private readonly List<byte> _receiveBuffer = [];
+
+    public DebugClient(ZeroMQLayer layer)
+    {
+        _layer = layer;
+        _layer.Wrap(this);
+    }
+
+    public string Capabilities()
+    {
+        SendCmd('?');
+        return ReceiveString();
+    }
+
+    public string[] List()
+    {
+        SendCmd('l');
+        string text = ReceiveString();
+        return text.Split('\n');
+    }
+
+    public string Echo(string text)
+    {
+        SendCmd('e', Encoding.ASCII.GetBytes(text));
+        return ReceiveString();
+    }
+
+    /// <summary>
+    /// Returns the value as Hex string.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    public string Read(string path)
+    {
+        SendCmd('r', Encoding.ASCII.GetBytes(path));
+        return ReceiveString();
+    }
+
+    public override void Decode(Span<byte> buffer)
+    {
+        _receiveBuffer.AddRange(buffer);
+        base.Decode(buffer);
+    }
+
+    private void SendCmd(char cmd, params byte[] data)
+    {
+        // Use stackalloc, since we send little data.
+        Span<byte> buffer = stackalloc byte[data.Length + 1];
+        buffer[0] = (byte)cmd;
+        data.CopyTo(buffer.Slice(1));
+        Encode(buffer, true);
+    }
+
+    private string ReceiveString()
+    {
+        int receivedBytes = _layer.ReceiveAll(Timeout);
+        var span = CollectionsMarshal.AsSpan(_receiveBuffer);
+        string text = Encoding.ASCII.GetString(span);
+        _receiveBuffer.Clear();
+        return text;
+    }
+}
--- examples/LibStored.Net.DebugClient/Program.cs
+++ examples/LibStored.Net.DebugClient/Program.cs
@@ -1,27 +1,35 @@
 ﻿// SPDX-FileCopyrightText: 2025 Guus Kuiper
-// 
+//
 // SPDX-License-Identifier: MIT
 
+using LibStored.Net.DebugClient;
+using LibStored.Net.ZeroMQ;
 using NetMQ;
 using NetMQ.Sockets;
 
+Console.WriteLine("Make sure the 8_sync example is running and uses the 5555 port for sync and 5556 for debugging. For example using the Dockerfile in the AppHost example:");
+Console.WriteLine("podman run -p 5555:5555 -p 5556:5556 <name>");
 {
     using RequestSocket socket = new();
     socket.Connect("tcp://localhost:5556");
+    DebugClient client = new(new ZeroMQLayer(socket));
 
-    string message = "?";
-    Console.WriteLine("<" + message);
-    socket.SendFrame(message);
-    string received = socket.ReceiveFrameString();
-    Console.WriteLine(">" + received);
+    string capabilities = client.Capabilities();
+    Console.WriteLine(capabilities);
+
+    string[] registers = client.List();
+    Console.WriteLine(string.Join("\n", registers));
+
+    string path = registers.Length == 0 ? "" : registers[0].Substring(registers[0].IndexOf('/'));
+    string value = client.Read(path);
+    Console.WriteLine($"{path} = {value}");
 
     foreach (int i in Enumerable.Range(0, 10))
     {
-        message = $"eHello {i:D2}";
+        string message = $"Hello {i:D2}";
         Console.WriteLine("<" + message);
-        socket.SendFrame(message);
-        received = socket.ReceiveFrameString();
-        Console.WriteLine("> " + received);
+        string received = client.Echo(message);
+        Console.WriteLine(">" + received);
     }
 }
 
