1a5ff586df120def60be6d1215b085518e762620
Guus Kuiper <guus.kuiper@demcon.com>
2025-10-02 12:57:40 +0200

    Add ArqLayer keepalive and event test

--- test/LibStored.Net.Tests/ArqLayerTests.cs
+++ test/LibStored.Net.Tests/ArqLayerTests.cs
@@ -2,6 +2,8 @@
 //
 // SPDX-License-Identifier: MIT
 
+using LibStored.Net.Protocol;
+
 namespace LibStored.Net.Tests;
 
 public class ArqLayerTests
@@ -9,10 +11,10 @@ public class ArqLayerTests
     [Fact]
     public void ArqNormalTest()
     {
-        Protocol.LoggingLayer top = new();
-        Protocol.ArqLayer arq = new();
+        LoggingLayer top = new();
+        ArqLayer arq = new();
         arq.Wrap(top);
-        Protocol.LoggingLayer bottom = new();
+        LoggingLayer bottom = new();
         bottom.Wrap(arq);
 
         top.Flush();
@@ -43,10 +45,10 @@ public class ArqLayerTests
     [Fact]
     public void RetransmitTest()
     {
-        Protocol.LoggingLayer top = new();
-        Protocol.ArqLayer arq = new();
+        LoggingLayer top = new();
+        ArqLayer arq = new();
         arq.Wrap(top);
-        Protocol.LoggingLayer bottom = new();
+        LoggingLayer bottom = new();
         bottom.Wrap(arq);
 
         bottom.Decode([0xff]);
@@ -99,5 +101,69 @@ public class ArqLayerTests
 
         bottom.Decode([0x02, .." 4"u8]);
         Assert.Equal(ProtocolTests.String([0x82]), bottom.Encoded[2]);
+    }
+
+    [Fact]
+    public void KeepAliveTest()
+    {
+        LoggingLayer top = new();
+        ArqLayer arq = new();
+        arq.Wrap(top);
+        LoggingLayer bottom = new();
+        bottom.Wrap(arq);
+
+        bottom.Decode([0x80, 0x40]);
+        arq.Flush();
+        bottom.Clear();
+
+        // No queue, empty message
+        arq.KeepAlive();
+        Assert.Equal(ProtocolTests.String([0x41]), bottom.Encoded[0]);
+        bottom.Decode([0x81]);
+
+        top.Encode(" 1"u8, true);
+        Assert.Equal(ProtocolTests.String([0x02, .." 1"u8]), bottom.Encoded[1]);
+
+        arq.KeepAlive();
+        Assert.Equal(ProtocolTests.String([0x02, .." 1"u8]), bottom.Encoded[2]); // Retransmit instead of empty message
+        bottom.Decode([0x82]);
+
+        bottom.Decode([0x41]);
+        Assert.Empty(top.Decoded);
+        Assert.Equal(ProtocolTests.String([0x81]), bottom.Encoded[3]); // Retransmit instead of empty message
+    }
+
+    [Fact]
+    public void EventTest()
+    {
+        LoggingLayer top = new();
+        ArqLayer arq = new(100);
+        arq.Wrap(top);
+        LoggingLayer bottom = new();
+        bottom.Wrap(arq);
+
+        ArqEvent lastEvent = ArqEvent.None;
+        arq.EventOccurred += (_, e) => lastEvent = e.Event;
+
+        bottom.Decode([0x80, 0x40]);
+        top.Encode(" 1"u8, true);
+        bottom.Decode([0x01]);
+        bottom.Clear();
+
+        Assert.Equal(ArqEvent.None, lastEvent);
+        bottom.Decode([0x40]);
+        Assert.Equal(ArqEvent.Reconnect, lastEvent);
+
+        for(int i = 0; i < 5; i++)
+        {
+            top.Encode("01234567890123456789"u8, true);
+        }
+        Assert.Equal(ArqEvent.EncodeBufferOverflow, lastEvent);
+
+        for (int i = 0; i < ArqLayer.RetransmitCallbackThreshold; i++)
+        {
+            top.Flush();
+        }
+        Assert.Equal(ArqEvent.Retransmit, lastEvent);
     }
 }
